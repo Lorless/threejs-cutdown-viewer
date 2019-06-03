39d370037b2267b2277e7c80e1fd0a1f766d7f62
lorless <dannylaws@hotmail.co.uk>
2019-06-03 10:31:07 +0100

    got the viewer displaying paths

--- index.js
+++ index.js
@@ -6,71 +6,78 @@ function init() {
     const ovcamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -2000, 10000);
     const objects = [];
     const camera = ovcamera;
-    camera.position.z = 100;
+    const loader = new THREE.SVGLoader();
 
     const renderer = new THREE.WebGLRenderer();
+    renderer.antialias = true;
+    renderer.setPixelRatio( window.devicePixelRatio );
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
+    camera.position.z = 100;
+
+    //load('test2.svg');
+    load('test3.svg');
+
+    //load('test3.svg');
+
+    function load(filename){
+        loader.load(filename, (data) => {
+
+                var paths = data.paths;
+                console.log(data);
+                var group = new THREE.Group();
+
+                console.log(paths);
+
+                for (let x = 0; x < paths.length; x++) {
 
 
-//cube
-    let mesh;
-    const cube = new THREE.BoxBufferGeometry(100, 100, 100);
-    mesh = new THREE.Mesh(cube, new THREE.MeshBasicMaterial({color: 0x0000ff, opacity: 0.5, transparent: true}));
-    mesh.position.x = 500;
-    mesh.scale.x = mesh.scale.y = mesh.scale.z = 2;
-    scene.add(mesh);
+                    for (var i = 0; i < paths[x].subPaths.length; i++) {
 
-// const path = new THREE.Path();
-//
-// path.lineTo( 0, 0.8 );
-// path.quadraticCurveTo( 0, 1, 0.2, 1 );
-// path.lineTo( 1, 1 );
-//
-// const points = path.getPoints();
-//
-// const geometry = new THREE.BufferGeometry().setFromPoints( points );
-// const material = new THREE.LineBasicMaterial( { color: 0xffffff } );
-//
-// const line = new THREE.Line( geometry, material );
-// scene.add( line );
+                        var path = paths[x].subPaths[i];
 
-    const loader = new THREE.SVGLoader();
-    loader.load('test2.svg', (data) => {
+                        const points = path.getPoints();
 
-            var paths = data.paths;
-            var group = new THREE.Group();
+                        console.log(path);
 
-            for ( var i = 0; i < paths.length; i ++ ) {
+                        const geometry = new THREE.BufferGeometry().setFromPoints(points);
+                        console.log(paths[x].userData);
+                        const material = new THREE.LineBasicMaterial({color: paths[x].userData.style.stroke});
 
-                var path = paths[ i ];
+                        const line = new THREE.Line(geometry, material);
+                        group.add(line);
 
-                var material = new THREE.MeshBasicMaterial( {
-                    color: path.color,
-                    side: THREE.DoubleSide,
-                    depthWrite: false
-                } );
 
-                var shapes = path.toShapes( true );
-
-                for ( var j = 0; j < shapes.length; j ++ ) {
-
-                    var shape = shapes[ j ];
-                    var geometry = new THREE.ShapeBufferGeometry( shape );
-                    var mesh = new THREE.Mesh( geometry, material );
-                    group.add( mesh );
-
+                    }
                 }
+                // console.log(group);
+                scene.add(group);
+            }, (progress) => {
 
-            }
-            console.log(group);
-            scene.add( group );
-        }, (progress) => {
-            console.log(progress);
-        },
-        (error) => {
-            console.log(error);
-        });
+            },
+            (error) => {
+                console.log(error);
+            });
+    }
+
+    var path = new THREE.Path();
+
+    path.lineTo( 100, 100 );
+    path.quadraticCurveTo( 0, 1, -200, 250 );
+    path.lineTo( 1, 1 );
+
+    var points = path.getPoints();
+
+    var geometry = new THREE.BufferGeometry().setFromPoints( points );
+    var material = new THREE.LineBasicMaterial( { color: 'red' } );
+
+    var line = new THREE.Line( geometry, material );
+    scene.add( line );
+
+
+
+
+
 
 
 // lighting
@@ -80,11 +87,13 @@ function init() {
 
 // render
     animate();
+
     function animate() {
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
         render();
     }
+
     function render() {
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
     }
 }
