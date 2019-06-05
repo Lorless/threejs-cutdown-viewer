6e1bf44170f6b0e9294678946550ad9730b83e52
danny laws <Danny@dannys-MacBook-Pro.local>
2019-06-05 09:05:22 +0100

    more investigation, text not working

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
 function init() {
 
 
+    //const ovcamera= new THREE.OrthographicCamera(left, right, bottom, top, near, far);
+
+    var options = {
+        ortho: true
+    };
+
+
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
-    const ovcamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -2000, 10000);
+    const ovcamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, -2000, 10000);
+    console.log(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -2000, 10000);
+    //const pscamera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
     const objects = [];
     const camera = ovcamera;
     const loader = new THREE.SVGLoader();
 
-    const renderer = new THREE.WebGLRenderer();
-    renderer.antialias = true;
+    console.log(camera);
+
+    let renderer = new THREE.WebGLRenderer();
+    renderer = new THREE.SVGRenderer();
+
+
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
-    camera.position.z = 100;
+    //camera.position.z = 1000;
 
     // for(let i =1; i <= 5; i++){
     //     load('svgs/'+i+'.svg');
@@ -27,7 +40,26 @@ function init() {
         doneFn(parsed);
     }
 
-    function doneFn(data) {
+    // for(let x = 0; x <=5; x++){
+    //     loader.load('svgs/'+x+'.svg', doneFn);
+    //     console.log('svgs/'+x+'.svg');
+    // }
+
+    // //loader.load('copyright.svg', doneFnWrapper);
+    // loader.load('dragon.svg', doneFnWrapperName);
+    // loader.load('cartman.svg', doneFnWrapper);
+
+    function doneFnWrapper(data) {
+
+        doneFn(data, true,)
+    }
+
+    function doneFnWrapperName(data) {
+        doneFn(data, true, 'dragon')
+    }
+
+    function doneFn(data, ignorePointAdjust, name) {
+
 
         var paths = data.paths;
 
@@ -44,10 +76,12 @@ function init() {
                 const points = path.getPoints();
 
 
-                points.forEach((point) => {
-                    point.x -= 300;
-                    point.y -= 300;
-                });
+                if (!ignorePointAdjust) {
+                    points.forEach((point) => {
+                        point.x -= 300;
+                        point.y -= 300;
+                    });
+                }
 
 
                 const geometry = new THREE.BufferGeometry().setFromPoints(points);
@@ -59,6 +93,11 @@ function init() {
 
 
             }
+        }
+
+        if (name === 'dragon') {
+            console.log('dragon', group);
+            group.scale.set(0.1, 0.1, 0.1)
         }
         // console.log(group);
         scene.add(group);
