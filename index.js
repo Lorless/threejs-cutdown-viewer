778ba956451367ce489463e1e4cb54936627ac8e
danny laws <Danny@dannys-MacBook-Pro.local>
2019-06-14 08:49:07 +0100

    image

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 function init() {
 
-
-    //const ovcamera= new THREE.OrthographicCamera(left, right, bottom, top, near, far);
-
-    var options = {
-        ortho: true
-    };
-
-
     const scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
-    const ovcamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, -2000, 10000);
-    //console.log(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -2000, 10000);
-    const pscamera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 1000 );
-    const camera = pscamera;
-
-
-    THREE.SVGLoader.prototype.myTestFunction = () => {
-        console.log('test');
-    };
     const loader = new THREE.SVGLoader();
-    loader.myTestFunction();
+
+    // flipped orthographic camera displays svgs in correct orientation
+    // const ovcamera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / -2, window.innerHeight / 2, 1, 1110);
+
+    // unflipped orthograpic camera displays plane with image correctly but svgs are flipped ?
+    const ovcamera = new THREE.OrthographicCamera(
+        window.innerWidth / -2, // frustum left plane
+        window.innerWidth / 2, // frustum right plane.
+        window.innerHeight / 2, // frustum top plane.
+        window.innerHeight / -2, // frustum bottom plane.
+        1, // frustum near plane.
+        110 // frustum far plane.
+    );
+    //perspective camera for testing
+    const pscamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1100);
+    let camera = pscamera;
+
+    camera.position.z = 1000;
+
 
     let webRenderer = new THREE.WebGLRenderer();
     let svgRenderer = new THREE.SVGRenderer();
-    let renderer = svgRenderer;
+    let renderer = webRenderer;
 
 
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -33,7 +34,6 @@ function init() {
     //add the canvas to the dom
     document.body.appendChild(renderer.domElement);
 
-    camera.position.z = 1000;
 
     // for(let i =1; i <= 5; i++){
     //     load('svgs/'+i+'.svg');
@@ -41,10 +41,11 @@ function init() {
 
     let displayables = json.series.viewList[0].drawingModel.currentDisplayables;
 
-    for (let x = 0; x < 1; x++) {
+    for (let x = 1; x < 6; x++) {
         let content = displayables[x].display.replace(/\n|\r/gi, "");
-        let parsed = loader.parse(content);
-        doneFn(parsed);
+        //let parsed = loader.parse(content);
+        loader.load('svgs/' + x + '.svg', doneFn);
+        //doneFn(parsed);
     }
 
     // for(let x = 0; x <=5; x++){
@@ -64,6 +65,8 @@ function init() {
     function doneFnWrapperName(data) {
         doneFn(data, true, 'dragon')
     }
+
+    let objects = [];
 
     function doneFn(data, ignorePointAdjust, name) {
 
@@ -85,8 +88,8 @@ function init() {
 
                 if (!ignorePointAdjust) {
                     points.forEach((point) => {
-                        point.x -= 300;
-                        point.y -= 300;
+                        point.x -= 150;
+                        point.y -= 150;
                     });
                 }
 
@@ -96,18 +99,21 @@ function init() {
                 const material = new THREE.LineBasicMaterial({color: paths[x].userData.style.stroke});
 
                 const line = new THREE.Line(geometry, material);
+
+                //line.rotation.x = 1;
+
                 group.add(line);
+
+
 
 
             }
         }
 
-        if (name === 'dragon') {
-            console.log('dragon', group);
-            group.scale.set(0.1, 0.1, 0.1)
-        }
         // console.log(group);
         scene.add(group);
+        objects.push(group);
+        group.rotation.x = 3.141;
     }
 
 
@@ -118,72 +124,63 @@ function init() {
 
     let material;
     let mesh;
+    let cube;
 
     console.log('textureLoader');
     let img = 'http://www.startradiology.com/uploads/images/english-class-x-hip-fig10-ap-view-lines-blanco.jpg';
     let ovImage = 'http://127.0.0.1:1337/stuart.adam.com/exam/0/image';
-    textureLoader.load(img, (texture)=>{
+    textureLoader.load('image.jfif', (texture) => {
         console.log('texture', texture);
-        material = new THREE.MeshBasicMaterial( {
+        material = new THREE.MeshBasicMaterial({
             map: texture
-        } );
+        });
+
         // create a plane geometry for the image with a width of 10
         // and a height that preserves the image's aspect ratio
-        let geometry = new THREE.PlaneGeometry(200, 200 * .75);
+        let geometry = new THREE.PlaneGeometry(200, 200);
 
         // combine our image geometry and material into a mesh
         mesh = new THREE.Mesh(geometry, material);
 
+
         // set the position of the image mesh in the x,y,z dimensions
         mesh.position.set(0, 0, 0);
+
 
         // add the image to the scene
         scene.add(mesh);
         console.log('mesh', mesh);
-    }, ()=>{
+    }, () => {
         console.log('progress')
-    }, (err)=>{
+    }, (err) => {
         console.log('failed!', err);
     });
 
 
-
-    // var path = new THREE.Path();
-    //
-    // path.lineTo(100, 100);
-    // path.quadraticCurveTo(0, 1, -200, 250);
-    // path.lineTo(1, 1);
-    //
-    // var points = path.getPoints();
-    //
-    // var geometry = new THREE.BufferGeometry().setFromPoints(points);
-    // var material = new THREE.LineBasicMaterial({color: 'red'});
-    //
-    // var line = new THREE.Line(geometry, material);
-    // scene.add(line);
-
-
-// lighting
+    // lighting
     const ambient = new THREE.AmbientLight(0x80ffff);
     scene.add(ambient);
     scene.add(camera);
 
-// render
+    // render
     animate();
 
+
     function animate() {
+
+        if(objects[0]){
+          //objects.forEach(obj=> obj.rotateX()
+            console.log(objects[0].rotation.x);
+        }
+
+
         requestAnimationFrame(animate);
         render();
     }
 
     function render() {
-        // if(mesh){
-        //     mesh.position.x +=1;
-        //     mesh.position.z +=1;
-        //     console.log(mesh.position);
-        // }
-
         renderer.render(scene, camera);
     }
+
 }
 
